f47b83b7ac50284c4066c85b44e1f8cb081195be
Keshv <keshv.s@utexas.edu>
2023-10-21 16:58:24 -0500

    react again

--- hacktx/src/components/mainpage.jsx
+++ hacktx/src/components/mainpage.jsx
@@ -12,6 +12,8 @@
   }
   ```
 */
+
+import Globe from "react-globe.gl";
 import { Fragment, useState } from "react";
 import { Dialog, Menu, Transition } from "@headlessui/react";
 import {
@@ -193,7 +195,7 @@ export default function MainPage() {
                               className="h-6 w-6 shrink-0 text-indigo-200 group-hover:text-white"
                               aria-hidden="true"
                             />
-                            Settings
+                            Reset
                           </a>
                         </li>
                       </ul>
@@ -208,7 +210,7 @@ export default function MainPage() {
         {/* Static sidebar for desktop */}
         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4">
+          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-600 px-6 pb-4">
             <div className="flex h-16 shrink-0 items-center">
               <img
                 className="h-8 w-auto"
@@ -267,19 +269,22 @@ export default function MainPage() {
                             />
                           </div>
                           <div className="ml-3 text-sm leading-6 flex items-center">
-                          <item.icon
-                                className={classNames(
-                                item.current ? "text-white" : "text-indigo-200 group-hover:text-white",
+                            <item.icon
+                              className={classNames(
+                                item.current
+                                  ? "text-white"
+                                  : "text-indigo-200 group-hover:text-white",
                                 "h-6 w-6 shrink-0 ml-2" // Adjust the margin as needed
-                                )}
-                                aria-hidden="true"
+                              )}
+                              aria-hidden="true"
                             />
-                            <label htmlFor={item.name} className="pl-5 font-medium text-white">
-                                {item.name}
+                            <label
+                              htmlFor={item.name}
+                              className="pl-5 font-medium text-white"
+                            >
+                              {item.name}
                             </label>
-                           
-                            </div>
-
+                          </div>
                         </div>
                       ))}
                     </div>
@@ -337,77 +342,33 @@ export default function MainPage() {
                   name="search"
                 />
               </form>
-              <div className="flex items-center gap-x-4 lg:gap-x-6">
-                <button
-                  type="button"
-                  className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
-                {/* Separator */}
-                <div
-                  className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10"
-                  aria-hidden="true"
-                />
-
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative">
-                  <Menu.Button className="-m-1.5 flex items-center p-1.5">
-                    <span className="sr-only">Open user menu</span>
-                    <img
-                      className="h-8 w-8 rounded-full bg-gray-50"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                    />
-                    <span className="hidden lg:flex lg:items-center">
-                      <span
-                        className="ml-4 text-sm font-semibold leading-6 text-gray-900"
-                        aria-hidden="true"
-                      >
-                        Tom Cook
-                      </span>
-                      <ChevronDownIcon
-                        className="ml-2 h-5 w-5 text-gray-400"
-                        aria-hidden="true"
-                      />
-                    </span>
-                  </Menu.Button>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                      {userNavigation.map((item) => (
-                        <Menu.Item key={item.name}>
-                          {({ active }) => (
-                            <a
-                              href={item.href}
-                              className={classNames(
-                                active ? "bg-gray-50" : "",
-                                "block px-3 py-1 text-sm leading-6 text-gray-900"
-                              )}
-                            >
-                              {item.name}
-                            </a>
-                          )}
-                        </Menu.Item>
-                      ))}
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              </div>
+              
             </div>
           </div>
 
-          <main className="py-10">
-            <div className="px-4 sm:px-6 lg:px-8">{/* Your content */}</div>
+          <main className="py-2">
+          <div className="flex justify-between">
+  <div className="w-2/3 px-4 sm:px-6 lg:px-8 py-10">
+    <div className="overflow-hidden rounded-lg bg-white shadow">
+      <div className="sm:p-6">
+        <Globe
+          width={800}
+          height={600}
+          backgroundColor="white"
+          globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        />
+      </div>
+    </div>
+  </div>
+  <div className="w-1/3 px-4 sm:px-6 lg:px-8 py-10">
+    <div className="overflow-hidden rounded-lg bg-gray-200 shadow">
+      <div className="px-4 py-5 sm:p-6">{/* Content goes here */}</div>
+    </div>
+
+
+  </div>
+</div>
+
           </main>
         </div>
       </div>
